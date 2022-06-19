90f7589b242608fd8c65b5dcc15668423cd0fc9a
zapyataya <53997666+zapyataya@users.noreply.github.com>
2022-06-19 21:08:03 +0500

    Update 2.py

--- Homework2/2.py
+++ Homework2/2.py
@@ -1,30 +1,48 @@
-#Список
+# Список: определить, какой из семей принадлежит фраза 'Winter is Coming')
 my_list = [
     ['Stark', 'Winter is Coming'],
     ['Lannister', 'A Lannister always pays his debts'],
     ['Greyjoy', 'What Is Dead May Never Die']
 ]
 
-#Словарь
+for i in my_list:
+    if i[1] == 'Winter is Coming':
+        print(i[0])
+
+# можно обратиться к нужному элементу по индексу
+# можно добавлять и удалять элементы
+# может содержать дубликаты
+# поддерживает вложенность (списки можно хранить внутри других списков)
+
+# Словарь: определить, какая фраза принадлежит семье Stark
 my_dict = {
     'Stark': 'Winter is Coming',
     'Lannister': 'A Lannister always pays his debts',
     'Greyjoy': 'What Is Dead May Never Die'
 }
 
-#Множество
-my_set = {'Winter is Coming', 'A Lannister always pays his debts', 'What Is Dead May Never Die'}
-
-#Задача - найти и вывести значение фразы семьи Старк
-
-#Решение задачи для списка
-for i in my_list:
-    if i[0] == 'Stark':
-        print(i[1])
-
-#Решение задачи для словаря
 print(my_dict['Stark'])
 
-#Для множества не нашла способ решить указанную задачу, так как элементы множества не упорядочены
+# у каждого значения словаря есть уникальный ключ, который выступает идентификатором элемента
+# можно добавлять и удалять элементы
+# нельзя вывести ключ с помощью значения
+# поддерживает вложенность
 
-#Для решения поставленной задачи лучше всего подходит словарь, так как для каждого значения можно указать ключ, с помощью которого легко можно найти нужное значение
+# Множество: определить, какой из семей принадлежит фраза 'Winter is Coming')
+my_set_Stark = {'Winter is Coming'}
+my_set_Lannister = {'A Lannister always pays his debts'}
+my_set_Greyjoy = {'What Is Dead May Never Die'}
+
+if 'Winter is Coming' in my_set_Stark:
+    print('Stark')
+if 'Winter is Coming' in my_set_Lannister:
+    print('Lannister')
+if 'Winter is Coming' in my_set_Greyjoy:
+    print('Greyjoy')
+
+# элементы множества не являются индексированными, нельзя обратиться к нужному элементу по индексу
+# можно добавлять и удалять элементы
+# не может содержать дубликаты
+# не поддерживает вложенность
+
+# наиболее удобным способом хранения считаю словарь, так как в нем есть уникальный идентификатор (ключ)
